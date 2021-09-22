b50913a7ce8a85a9c9ca6e7c0998918c8081d402
Jonathan Cardenas <jonscardenas@gmail.com>
2021-09-22 00:24:26 -0500

    added validation to inputs

--- javascript.js
+++ javascript.js
@@ -13,98 +13,298 @@ const submitButton = document.getElementById('submitButton');
 function validate(e){
     e.preventDefault();
 
+    //clear validation error messages
+    //document.getElementsByClassName('validation-error').innerHTML = '';
+
 
     console.log('form submitted');
     //console.log(form.elements);
 
     //let inputs = form.elements;
-    //let typeOfIncident = ;
+    
 
     //validation
     //get injury or illness
 
-    /*if (inputs['injury']){
-        typeOfIncident = injury;
-    }else if (inputs['illness']){
-        typeOfIncident = illness;
+    
+
+    let injuryIncident = document.getElementById('injuryIncident');
+    let illnessIncident = document.getElementById('illnessIncident');
+    let incidentTypeErrorMessage = document.getElementById('incidentTypeErrorMessage');
+    let incidentTypeLabel = document.getElementById('incidentTypeLabel');
+    incidentTypeErrorMessage.innerHTML = '';
+
+    if (injuryIncident.checked === false && illnessIncident.checked === false){
+        //incidentTypeErrorMessage.innerHTML = 'please select incident type';
+        //incidentTypeErrorMessage.focus();
+        incidentTypeLabel.classList.add('validation-error');
+        incidentTypeLabel.focus();
+        //alert('Please select an incident type');
+        return false;
     }else{
-        console.log('please select the type of incident');
-    }*/
+        incidentTypeLabel.classList.remove('validation-error');
+    }
 
-    let injuryIncident = document.getElementById('injuryIncident').checked;
-    let illnessIncident = document.getElementById('illnessIncident').checked;
+    let name = document.getElementById('name');
+    let nameErrorMessage = document.getElementById('nameErrorMessage')
+    let nameLabel = document.getElementById('nameLabel');
 
-    console.log('injury or illness');
-    console.log(injuryIncident);
-    console.log(illnessIncident);
+    console.log(`name: ${name.value}`);
 
-    let name = document.getElementById('name').value;
+    if (name.value == ""){
+        //nameErrorMessage.innerHTML = 'please enter your name';
+        nameLabel.classList.add('validation-error');
+        nameLabel.focus();
+        //alert('Please enter your name');
+        return false;
+    }else{
+        //nameErrorMessage.innerHTML = '';
+        nameLabel.classList.remove('validation-error');
 
-    console.log(`name: ${name}`);
 
-    let jobTitle = document.getElementById('jobTitle').value;
+    }
 
-    console.log(`job title: ${jobTitle}`);
+    let jobTitle = document.getElementById('jobTitle');
+    let jobTitleErrorMessage = document.getElementById('jobTitleErrorMessage');
+    let jobTitleLabel = document.getElementById('jobTitleLabel');
 
-    let supervisor = document.getElementById('supervisor').value;
-    console.log(`super visor: ${supervisor}`);
+    console.log(`job title: ${jobTitle.value}`);
 
-    let whoDidYourReportTo = document.getElementById('whoDidYourReportTo').value;
-    console.log(`whoDidYourReportTo: ${whoDidYourReportTo}`);
+    if (jobTitle.value == ""){
+        //jobTitleErrorMessage.innerHTML = 'please enter your job title';
+        jobTitleLabel.classList.add('validation-error');
+        jobTitleLabel.focus();
+        //alert('Please enter your name');
+        return false;
+    }else{
+        jobTitleLabel.classList.remove('validation-error');
+    }
 
-    let incidentDate = document.getElementById('incidentDate').value;
-    console.log(`incidentDate: ${incidentDate}`);
 
-    let incidentTime = document.getElementById('incidentTime').value;
-    console.log(`incidentTime: ${incidentTime}`);
+    let supervisor = document.getElementById('supervisor');
+    let supervisorLabel = document.getElementById('supervisorLabel');
 
-    let shiftStartTime = document.getElementById('shiftStartTime').value;
+    console.log(`super visor: ${supervisor.value}`);
+
+    if (supervisor.value == ""){
+        //jobTitleErrorMessage.innerHTML = 'please enter your job title';
+        supervisorLabel.classList.add('validation-error');
+        supervisorLabel.focus();
+        //alert('Please enter your name');
+        return false;
+    }else{
+        supervisorLabel.classList.remove('validation-error');
+    }
+
+
+    let whoDidYouReportTo = document.getElementById('whoDidYouReportTo');
+    let whoDidYouReportToLabel = document.getElementById('whoDidYouReportToLabel');
+
+    console.log(`whoDidYouReportTo: ${whoDidYouReportTo.value}`);
+    
+    if (whoDidYouReportTo.value == ""){
+        //jobTitleErrorMessage.innerHTML = 'please enter your job title';
+        whoDidYouReportToLabel.classList.add('validation-error');
+        whoDidYouReportToLabel.focus();
+        //alert('Please enter your name');
+        return false;
+    }else{
+        whoDidYouReportToLabel.classList.remove('validation-error');
+    }
+
+
+
+    let incidentDate = document.getElementById('incidentDate');
+    let incidentDateLabel = document.getElementById('incidentDateLabel');
+
+    console.log(`incidentDate: ${incidentDate.value}`);
+
+    if (incidentDate.value == ""){
+        incidentDateLabel.classList.add('validation-error');
+        incidentDateLabel.focus();
+        return false;
+    }else{
+        incidentDateLabel.classList.remove('validation-error');
+    }
+
+
+
+    let incidentTime = document.getElementById('incidentTime');
+    let incidentTimeLabel = document.getElementById('incidentTimeLabel');
+
+    console.log(`incidentTime: ${incidentTime.value}`);
+
+    if (incidentTime.value == ""){
+        incidentTimeLabel.classList.add('validation-error');
+        incidentTimeLabel.focus();
+        return false;
+    }else{
+        incidentTimeLabel.classList.remove('validation-error');
+    }
+
+
+
+    let shiftStartTime = document.getElementById('shiftStartTime');
+    let shiftStartTimeLabel = document.getElementById('shiftStartTimeLabel');
+
     console.log(`shiftStartTime: ${shiftStartTime}`);
 
-    let witnesses = document.getElementById('witnesses').value;
+    if (shiftStartTime.value == ""){
+        shiftStartTimeLabel.classList.add('validation-error');
+        shiftStartTimeLabel.focus();
+        return false;
+    }else{
+        shiftStartTimeLabel.classList.remove('validation-error');
+    }
+
+    /*let witnesses = document.getElementById('witnesses');
+    let witnessesLabel = document.getElementById('witnessesLabel');
+
     console.log(`witnesses: ${witnesses}`);
 
-    let incidentLocation = document.getElementById('incidentLocation').value;
+    if (witnesses.value == ""){
+        witnessesLabel.classList.add('validation-error');
+        witnessesLabel.focus();
+        return false;
+    }else{
+        witnessesLabel.classList.remove('validation-error');
+    }*/
+
+    let incidentLocation = document.getElementById('incidentLocation');
+    let incidentLocationLabel = document.getElementById('incidentLocationLabel');
+
     console.log(`incidentLocation: ${incidentLocation}`);
 
-    let whatWereYouDoing = document.getElementById('whatWereYouDoing').value;
+    if (incidentLocation.value == ""){
+        incidentLocationLabel.classList.add('validation-error');
+        incidentLocationLabel.focus();
+        return false;
+    }else{
+        incidentLocationLabel.classList.remove('validation-error');
+    }
+
+    let whatWereYouDoing = document.getElementById('whatWereYouDoing');
+    let whatWereYouDoingLabel = document.getElementById('whatWereYouDoingLabel');
+
     console.log(`whatWereYouDoing: ${whatWereYouDoing}`);
+    
+    if (whatWereYouDoing.value == ""){
+        whatWereYouDoingLabel.classList.add('validation-error');
+        whatWereYouDoingLabel.focus();
+        return false;
+    }else{
+        whatWereYouDoingLabel.classList.remove('validation-error');
+    }
 
-    let whatHappened = document.getElementById('whatHappened').value;
+    let whatHappened = document.getElementById('whatHappened');
+    let whatHappenedLabel = document.getElementById('whatHappenedLabel');
+
     console.log(`whatHappened: ${whatHappened}`);
+    
+    if (whatHappened.value == ""){
+        whatHappenedLabel.classList.add('validation-error');
+        whatHappenedLabel.focus();
+        return false;
+    }else{
+        whatHappenedLabel.classList.remove('validation-error');
+    }
 
-    let bodyPartInjured = document.getElementById('bodyPartInjured').value;
+
+    let bodyPartInjured = document.getElementById('bodyPartInjured');
+    let bodyPartInjuredLabel = document.getElementById('bodyPartInjuredLabel');
+
     console.log(`bodyPartInjured: ${bodyPartInjured}`);
 
-    let wasTreatedYes = document.getElementById('wasTreatedYes').checked;
-    console.log(`wasTreatedYes: ${wasTreatedYes}`);
+    if (bodyPartInjured.value == ""){
+        bodyPartInjuredLabel.classList.add('validation-error');
+        bodyPartInjuredLabel.focus();
+        return false;
+    }else{
+        bodyPartInjuredLabel.classList.remove('validation-error');
+    }
 
-    let wasTreatedNo = document.getElementById('wasTreatedNo').checked;
-    console.log(`wasTreatedNo: ${wasTreatedNo}`);
 
-    let treatedBy = document.getElementById('treatedBy').value;
+
+
+    let wasTreatedYes = document.getElementById('wasTreatedYes');
+    console.log(`wasTreatedYes: ${wasTreatedYes.value}`);
+
+    let wasTreatedNo = document.getElementById('wasTreatedNo');
+    console.log(`wasTreatedNo: ${wasTreatedNo.value}`);
+
+    let wasTreatedLabel = document.getElementById('wasTreatedLabel');
+
+    if (wasTreatedYes.checked === false && wasTreatedNo.checked === false){
+        //incidentTypeErrorMessage.innerHTML = 'please select incident type';
+        //incidentTypeErrorMessage.focus();
+        wasTreatedLabel.classList.add('validation-error');
+        wasTreatedLabel.focus();
+        //alert('Please select an incident type');
+        return false;
+    }else{
+        wasTreatedLabel.classList.remove('validation-error');
+    }
+
+
+
+    /*let treatedBy = document.getElementById('treatedBy');
     console.log(`treatedBy: ${treatedBy}`);
 
-    let treatedByPhone = document.getElementById('treatedByPhone').value;
+    let treatedByPhone = document.getElementById('treatedByPhone');
     console.log(`treatedByPhone: ${treatedByPhone}`);
 
-    let placeOfTreatment = document.getElementById('placeOfTreatment').value;
-    console.log(`placeOfTreatment: ${placeOfTreatment}`);
+    let placeOfTreatment = document.getElementById('placeOfTreatment');
+    console.log(`placeOfTreatment: ${placeOfTreatment}`);*/
 
-    let injuredBeforeYes = document.getElementById('injuredBeforeYes').checked;
+    let injuredBeforeYes = document.getElementById('injuredBeforeYes');
     console.log(`injuredBeforeYes: ${injuredBeforeYes}`);
 
-    let injuredBeforeNo = document.getElementById('injuredBeforeNo').checked;
+    let injuredBeforeNo = document.getElementById('injuredBeforeNo');
     console.log(`injuredBeforeNo: ${injuredBeforeNo}`);
 
-    let priorInjuryDescription = document.getElementById('priorInjuryDescription').value;
-    console.log(`priorInjuryDescription: ${priorInjuryDescription}`);
+    let injuredBeforeLabel = document.getElementById('injuredBeforeLabel');
 
-    let signature = document.getElementById('signature').value;
+    if (injuredBeforeYes.checked === false && injuredBeforeNo.checked === false){
+        //incidentTypeErrorMessage.innerHTML = 'please select incident type';
+        //incidentTypeErrorMessage.focus();
+        injuredBeforeLabel.classList.add('validation-error');
+        injuredBeforeLabel.focus();
+        //alert('Please select an incident type');
+        return false;
+    }else{
+        injuredBeforeLabel.classList.remove('validation-error');
+    }
+
+    /*
+    let priorInjuryDescription = document.getElementById('priorInjuryDescription');
+    console.log(`priorInjuryDescription: ${priorInjuryDescription}`);*/
+    
+
+    let signature = document.getElementById('signature');
+    let signatureLabel = document.getElementById('signatureLabel');
+
     console.log(`signature: ${signature}`);
 
-    let todaysDate = document.getElementById('todaysDate').value;
+    if (signature.value == ""){
+        signatureLabel.classList.add('validation-error');
+        signatureLabel.focus();
+        return false;
+    }else{
+        signatureLabel.classList.remove('validation-error');
+    }
+
+    let todaysDate = document.getElementById('todaysDate');
+    let todaysDateLabel = document.getElementById('todaysDateLabel');
+
     console.log(`todaysDate: ${todaysDate}`);
+
+    if (todaysDate.value == ""){
+        todaysDateLabel.classList.add('validation-error');
+        todaysDateLabel.focus();
+        return false;
+    }else{
+        todaysDateLabel.classList.remove('validation-error');
+    }
 
 
 }
@@ -116,3 +316,8 @@ submitButton.addEventListener('click', validate);
 
 
 
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+});
